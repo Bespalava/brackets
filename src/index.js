6e3a0bf6a287792e4eac9495a650161eeb7b2c0f
ybespalava <ybespalava@gmail.com>
2021-03-13 23:05:05 +0300

    feat: add function solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var St = str;
+    for (let i = 0; i <= str.length; i++) {
+        for (let j = 0; j <= bracketsConfig.length; j++) {
+            St = St.replace(bracketsConfig.map((item) => item.join(''))[j], '')
+        }
+    }
+    if (St.length == 0) {
+        return true
+    } else {
+        return false
+    }
 }
